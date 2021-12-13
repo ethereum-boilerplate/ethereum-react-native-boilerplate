cd8210e298861f3acc428cfaff800b65a79daa50
Malik Abd <mohammedabdmalik@gmail.com>
2021-12-13 00:19:52 -0600

    feat(seperated the native balance component and usenativebalance hook): useNativeBalance created

--- frontend/Components/Assets/NativeBalance.js
+++ frontend/Components/Assets/NativeBalance.js
@@ -3,6 +3,7 @@ import { useMoralis, useMoralisWeb3Api } from "react-moralis";
 import { View, Text, StyleSheet } from "react-native";
 import { useMoralisDapp } from "../../providers/MoralisDappProvider/MoralisDappProvider";
 import { n4 } from "../../utils/formatters";
+import useNativeBalance from "../../hooks/useNativeBalance";
 
 import { getNativeByChain } from "../../utils/getNativeByChain";
 
@@ -10,45 +11,13 @@ function NativeBalance(props) {
   const { account } = useMoralisWeb3Api();
   const { Moralis } = useMoralis();
   const { walletAddress, chainId: chain } = useMoralisDapp();
-  const [nativeBalance, setNativeBalance] = useState();
   const [nativeChainString, setNativeChainString] = useState();
 
-  const [address, setAddress] = useState();
-
-  useEffect(() => {
-    setAddress(walletAddress);
-  }, [walletAddress]);
-
-  const fetchNativeBalance = async () => {
-    const chainFinal = props?.chain || chainId;
-    const options = { address, chainFinal };
-
-    const native = getNativeByChain(chainFinal);
-
-    account
-      .getNativeBalance(options)
-      .then((result) => {
-        const balanceInWei = Moralis.Units.FromWei(result.balance);
-        const balanceFormatted = `${n4.format(balanceInWei)} ${native}`;
-        console.log(result, "RESULT");
-        setNativeChainString(native);
-        setNativeBalance(balanceFormatted);
-      })
-      .catch((e) => alert(e.message));
-  };
-
-  useEffect(() => {
-    if (address && chain) {
-      fetchNativeBalance();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [address, chain]);
+  const { nativeBalance } = useNativeBalance(props?.chain || chainId);
 
   return (
     <View style={styles.itemView}>
-      <Text style={styles.name}>
-        {nativeChainString}💰{nativeBalance}{" "}
-      </Text>
+      <Text style={styles.name}>💰 {nativeBalance} </Text>
     </View>
   );
 }
@@ -70,10 +39,6 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: "black",
     fontWeight: "500",
-  },
-  nameView: {
-    flex: 1,
-    alignItems: "flex-end",
   },
 });
 
