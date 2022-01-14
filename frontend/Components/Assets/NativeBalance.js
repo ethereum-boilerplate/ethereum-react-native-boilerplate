e9c31bca3171ad2d4779b32a5dfb152f93c9ec42
Malik Abd <mohammedabdmalik@gmail.com>
2022-01-14 10:47:12 -0600

    Fixed bug cant find variable

--- frontend/Components/Assets/NativeBalance.js
+++ frontend/Components/Assets/NativeBalance.js
@@ -1,17 +1,19 @@
-import { View, Text, StyleSheet } from "react-native";
-import { useMoralisDapp } from "../../providers/MoralisDappProvider/MoralisDappProvider";
-import { n4 } from "../../utils/formatters";
-import useNativeBalance from "../../hooks/useNFTBalance";
+import React, {useEffect, useState} from 'react';
+import {useMoralis, useMoralisWeb3Api} from 'react-moralis';
+import {View, Text, StyleSheet} from 'react-native';
+import {useMoralisDapp} from '../../providers/MoralisDappProvider/MoralisDappProvider';
+import {n4} from '../../utils/formatters';
+import useNativeBalance from '../../hooks/useNFTBalance';
 
-import { getNativeByChain } from "../../utils/getNativeByChain";
+import {getNativeByChain} from '../../utils/getNativeByChain';
 
 function NativeBalance(props) {
-  const { account } = useMoralisWeb3Api();
-  const { Moralis } = useMoralis();
-  const { walletAddress, chainId: chain } = useMoralisDapp();
+  const {account} = useMoralisWeb3Api();
+  const {Moralis} = useMoralis();
+  const {walletAddress, chainId: chain} = useMoralisDapp();
   const [nativeChainString, setNativeChainString] = useState();
 
-  const { nativeBalance } = useNativeBalance(props?.chain || chainId);
+  const {nativeBalance} = useNativeBalance(props?.chain || chainId);
 
   return (
     <View style={styles.itemView}>
@@ -21,21 +23,21 @@ function NativeBalance(props) {
 }
 const styles = StyleSheet.create({
   itemView: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     padding: 20,
     // marginVertical: 8,
     marginHorizontal: 2,
     marginVertical: 5,
     borderRadius: 10,
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   name: {
     fontSize: 15,
-    color: "black",
-    fontWeight: "500",
+    color: 'black',
+    fontWeight: '500',
   },
 });
 
