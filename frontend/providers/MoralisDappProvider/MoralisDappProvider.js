6a1e3dd1e9e7878a007685a93c902bb06cbf0623
Malik Abd <mohammedabdmalik@gmail.com>
2022-03-21 12:00:12 -0500

    Fixed typo

--- frontend/providers/MoralisDappProvider/MoralisDappProvider.js
+++ frontend/providers/MoralisDappProvider/MoralisDappProvider.js
@@ -11,7 +11,7 @@ function MoralisDappProvider({children}) {
       setChainId(chain);
     });
 
-    Moralis.onAccountsChanged(function (address) {
+    Moralis.onAccountChanged(function (address) {
       setWalletAddress(address[0]);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
